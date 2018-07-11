691d267a5277ad9540a34c821eebd16ea43d1df0
zs <1967376524@qq.com>
2018-07-11 22:43:12 +0800

    add all

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -62,7 +62,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: '/assets/',
+  publicPath: 'react-gallery/assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
